1110ff2629a190d11d18d3a80f4e27a8ff647fd8
samuelctl <samuel.s.aguiar@aluno.senai.br>
2026-02-20 19:37:23 -0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "postgresql+psycopg2://postgres:81848938spM!@localhost:5432/db_crud"
+DATABASE_URL = "postgresql+psycopg2://postgres:**********@localhost:5432/db_crud"
 
 engine = create_engine( DATABASE_URL,echo=True)
 
